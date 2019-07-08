46fe50280335ee09dc5a86288464d0f79a0f4edb
GQSM <ms314006@hotmail.com>
2019-07-09 07:43:13 +0800

    [CHANGE] 將圓形抽出來成一個 Component ，並替換開始按鈕為一個 Icon

--- src/component/Main/Information/Information.js
+++ src/component/Main/Information/Information.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import AddTodoInput from '../../AddTodoInput';
+import Circle from '../../Circle';
 import styles from './index.scss';
 
 const Information = (props) => {
@@ -7,7 +9,18 @@ const Information = (props) => {
     <div
       className={styles.information_block}
       data-testid="information_block"
-    />
+    >
+      <AddTodoInput />
+      <div className={styles.current_todo_block}>
+        <Circle style={{ border: '1px solid #003164', width: '48px', height: '48px', }} />
+        <div className={styles.todo_information_block}>
+          <span>Current_todo</span>
+          <div className={styles.current_tomato_block}>
+            <Circle />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
