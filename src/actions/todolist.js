c8e24d336e15947e43fc960fa9e8d49c85623151
GQSM <ms314006@hotmail.com>
2019-07-09 22:33:17 +0800

    [ADD] 增加 actions ，分別是 增加 todo ，增加執行 todo 的秒數，確認完成

---
+++ src/actions/todolist.js
@@ -0,0 +1,27 @@
+export const ADD_TODO = 'ADD_TODO';
+
+export const addTodoList = todo => ({
+  type: ADD_TODO,
+  payload: {
+    todo,
+  },
+});
+
+export const ADD_SPEND_SECONDS = 'ADD_SPEND_SECONDS';
+
+export const addSpendSeconds = id => ({
+  type: ADD_SPEND_SECONDS,
+  payload: {
+    id,
+  },
+});
+
+export const CHECK_TODO = 'CHECK_TODO';
+
+export const checkTodo = ({ id, completed, }) => ({
+  type: CHECK_TODO,
+  payload: {
+    id,
+    completed,
+  },
+});
---
+++ test/actions/todolist.test.js
@@ -0,0 +1,55 @@
+import * as actions from '../../src/actions/todolist';
+
+const todo = {
+  name: 'TodoList 2',
+  completed: false,
+  spendSeconds: 0,
+};
+
+describe('test actions', () => {
+  test('Add todo', () => {
+    expect(actions.ADD_TODO).toBe('ADD_TODO');
+
+    const expectAction = {
+      type: actions.ADD_TODO,
+      payload: {
+        todo: {
+          ...todo,
+        },
+      },
+    };
+
+    expect(actions.addTodoList(todo)).toEqual(expectAction);
+  });
+
+  test('Add spend seconds', () => {
+    expect(actions.ADD_SPEND_SECONDS).toBe('ADD_SPEND_SECONDS');
+
+    const expectAction = {
+      type: actions.ADD_SPEND_SECONDS,
+      payload: {
+        id: 1,
+      },
+    };
+
+    expect(actions.addSpendSeconds(1)).toEqual(expectAction);
+  });
+
+  test('Check todo completed', () => {
+    expect(actions.CHECK_TODO).toBe('CHECK_TODO');
+
+    const expectAction = {
+      type: actions.CHECK_TODO,
+      payload: {
+        id: 1,
+        completed: true,
+      },
+    };
+
+    expect(actions.checkTodo({ id: 1, completed: true, })).toEqual(expectAction);
+
+    expectAction.payload.completed = false;
+    expect(actions.checkTodo({ id: 1, completed: false, })).toEqual(expectAction);
+  });
+
+});
