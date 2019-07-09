672cd2be56ddec4f3367a5215ba4379267b00bfa
GQSM <ms314006@hotmail.com>
2019-07-09 22:37:02 +0800

    [ADD] 增加主頁左邊的資料區塊

--- src/component/Main/Information/Information.js
+++ src/component/Main/Information/Information.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import AddTodoInput from '../../AddTodoInput';
+import Time from '../../Time';
+import Circle from '../../Circle';
 import styles from './index.scss';
 
-const Information = (props) => {
-  const { } = props;
-  return (
-    <div
-      className={styles.information_block}
-      data-testid="information_block"
-    />
-  );
-};
+const Information = () => (
+  <div
+    className={styles.information_block}
+    data-testid="information_block"
+  >
+    <AddTodoInput />
+    <div className={styles.center_block}>
+      <div className={styles.current_todo_block}>
+        <Circle style={{ border: '1px solid #003164', width: '48px', height: '48px', }} />
+        <div className={styles.todo_information_block}>
+          <span>Current_todo</span>
+          <div className={styles.current_tomato_block}>
+            <Circle style={{
+              backgroundColor: '#003164',
+              width: '13px',
+              height: '13px',
+              marginRight: '8px',
+            }}
+            />
+            <Circle style={{ border: '1px solid #FF4384', width: '12px', height: '12px', }} />
+          </div>
+        </div>
+      </div>
+      <Time />
+    </div>
+  </div>
+);
 
 export default Information;
