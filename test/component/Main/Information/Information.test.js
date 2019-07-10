552cd6c196dbc41653d7277cffe0b11ad510d76f
GQSM <ms314006@hotmail.com>
2019-07-10 22:37:17 +0800

    [CHANGE] 導入 Redux 所以用不同的 Render 方式

--- test/component/Main/Information/Information.test.js
+++ test/component/Main/Information/Information.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { render, cleanup } from '@testing-library/react';
 import { toBeInTheDocument } from 'jest-dom';
 import Information from '../../../../src/component/Main/Information';
+import reducer from '../../../../src/reducer/todolist';
 
 expect.extend({ toBeInTheDocument, });
+
+const renderWithRedux = (ui, { initialState, store = createStore(reducer, initialState), } = {}) => ({
+  ...render(<Provider store={store}>{ui}</Provider>),
+  store,
+});
 
 describe('<Information />', () => {
   afterEach(() => {
@@ -11,7 +19,7 @@ describe('<Information />', () => {
   });
 
   test('是否正常 render', () => {
-    const { getByTestId, } = render(<Information />);
+    const { getByTestId, } = renderWithRedux(<Information />);
     expect(getByTestId('information_block')).toBeInTheDocument();
   });
 });
