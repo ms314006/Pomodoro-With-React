046b180e8f1b08307bba331519f7700973fb5c40
GQSM <ms314006@hotmail.com>
2019-07-10 22:33:40 +0800

    [ADD] 導入 Redux 管理管理 State

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import Information from './Information';
 import TimeController from './TimeController';
 import Menu from './Menu';
+import store from '../../store';
 import styles from './index.scss';
 
 const Main = () => (
-  <div
-    className={styles.main_block}
-    data-testid="main_block"
-  >
-    <Information />
-    <TimeController />
-    <Menu />
-  </div>
+  <Provider store={store}>
+    <div
+      className={styles.main_block}
+      data-testid="main_block"
+    >
+      <Information />
+      <TimeController />
+      <Menu />
+    </div>
+  </Provider>
 );
 
 export default Main;
