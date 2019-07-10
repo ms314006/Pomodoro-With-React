c841ee963cf86a27fcd292bc65d29d571da9700b
GQSM <ms314006@hotmail.com>
2019-07-10 22:37:56 +0800

    [ADD] 完成 AddTodoInput 和測試案例

--- src/component/AddTodoInput/AddTodoInput.js
+++ src/component/AddTodoInput/AddTodoInput.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addTodo } from '../../actions/todolist';
 import styles from './index.scss';
 
-const AddTodoInput = () => (
-  <div
-    className={styles.add_todo_input_block}
-    data-testid="add_todo_input_block"
-  >
-    <input
-      className={styles.add_todo_input}
-      value="ADD A NEW MISSION..."
-    />
-    <i className={`fas fa-plus ${styles.add_todo_icon}`} />
-  </div>
-);
+const AddTodoInput = (props) => {
+  const [todoName, setTodoName] = useState('');
+  return (
+    <div
+      className={styles.add_todo_input_block}
+      data-testid="add_todo_input_block"
+    >
+      <input
+        value={todoName}
+        className={styles.add_todo_input}
+        placeholder="ADD A NEW MISSION..."
+        onChange={(e) => {
+          setTodoName(e.target.value);
+        }}
+        data-testid="add_todo_input"
+      />
+      <i
+        className={`fas fa-plus ${styles.add_todo_icon}`}
+        onClick={() => {
+          props.addTodo(todoName);
+          setTodoName('');
+        }}
+        onKeyDown={() => {}}
+        data-testid="add_todo_button"
+      />
+    </div>
+  );
+};
 
-export default AddTodoInput;
+AddTodoInput.propTypes = {
+  addTodo: PropTypes.func,
+};
+
+AddTodoInput.defaultProps = {
+  addTodo: () => {},
+};
+
+const mapStateToDispatch = dispatch => ({
+  addTodo: (name) => { dispatch(addTodo({ name, })); },
+});
+
+export default connect(null, mapStateToDispatch)(AddTodoInput);
