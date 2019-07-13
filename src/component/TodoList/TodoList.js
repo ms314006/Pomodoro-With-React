6ea38178f8ee7f635db5f6e8da433eda9c652207
GQSM <ms314006@hotmail.com>
2019-07-13 21:28:30 +0800

    [ADD] 為 TodoList 增加了 listCount 的 Props 控制顯示幾筆，也有測試案例，然後 listName 寬度 100% 會擠到圈圈，所以改成 90%

--- src/component/TodoList/TodoList.js
+++ src/component/TodoList/TodoList.js
@@ -5,23 +5,25 @@ import Todo from './Todo';
 import styles from './index.scss';
 
 const TodoList = (props) => {
-  const { todoList, } = props;
+  const { todoList, listCount, } = props;
   return (
     <div
       className={styles.todoList_block}
       data-testid="todoList_block"
     >
-      { todoList.map(todo => <Todo key={todo.id} todo={todo} />) }
+      { todoList.slice(0, (listCount || todoList.length)).map(todo => <Todo key={todo.id} todo={todo} />) }
     </div>
   );
 };
 
 TodoList.propTypes = {
   todoList: PropTypes.arrayOf(PropTypes.shape({})),
+  listCount: PropTypes.number,
 };
 
 TodoList.defaultProps = {
   todoList: [],
+  listCount: 0,
 };
 
 const mapStateToProps = state => ({
