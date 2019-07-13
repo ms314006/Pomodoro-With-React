e60ea39afc7e571ee455c779b8c17063bc906756
GQSM <ms314006@hotmail.com>
2019-07-13 21:29:44 +0800

    [ADD] 加上 router 控制頁面內容

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import Information from './Information';
 import TimeController from './TimeController';
 import Menu from './Menu';
 import store from '../../store';
 import styles from './index.scss';
 
+const HomePage = () => (
+  <>
+    <Information />
+    <TimeController />
+    <Menu />
+  </>
+);
+
 const Main = () => (
   <Provider store={store}>
-    <div
-      className={styles.main_block}
-      data-testid="main_block"
-    >
-      <Information />
-      <TimeController />
-      <Menu />
-    </div>
+    <HashRouter>
+      <div
+        className={styles.main_block}
+        data-testid="main_block"
+      >
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </div>
+    </HashRouter>
   </Provider>
 );
 
