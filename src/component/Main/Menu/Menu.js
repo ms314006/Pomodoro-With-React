11fa935fb68a9f67eefe7008ef6f8051fb974856
GQSM <ms314006@hotmail.com>
2019-07-13 21:44:17 +0800

    [ADD] 完成 Menu 的內容及測試案例

--- src/component/Main/Menu/Menu.js
+++ src/component/Main/Menu/Menu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './index.scss';
 
 const Menu = (props) => {
@@ -7,7 +8,49 @@ const Menu = (props) => {
     <div
       className={styles.menu_block}
       data-testid="menu_block"
-    />
+    >
+      <div
+        className={styles.menu_list_block}
+        data-testid="menu_list_block"
+      >
+        <div>
+          <Link
+            to="/todolist"
+            className={styles.menu_icon}
+            data-testid="menu_icon_todlist"
+          >
+            <i className="fas fa-list" />
+          </Link>
+        </div>
+        <div>
+          <Link
+            to="/todolist/chart"
+            className={styles.menu_icon}
+            data-testid="menu_icon_chart"
+          >
+            <i className="fas fa-poll" />
+          </Link>
+        </div>
+        <div>
+          <Link
+            to="/todolist/ring"
+            className={styles.menu_icon}
+            data-testid="menu_icon_ring"
+          >
+            <i className="fab fa-itunes-note" />
+          </Link>
+        </div>
+      </div>
+      <div />
+      <div
+        className={styles.logo_block}
+        data-testid="logo_block"
+      >
+        <span className={styles.logo}>
+          POMODORO
+        </span>
+      </div>
+    </div>
   );
 };
 
--- test/component/Main/Menu/Menu.test.js
+++ test/component/Main/Menu/Menu.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router, HashRouter } from 'react-router-dom';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 import { toBeInTheDocument } from 'jest-dom';
 import Menu from '../../../../src/component/Main/Menu';
 
@@ -11,7 +13,33 @@ describe('<Menu />', () => {
   });
 
   test('是否正常 render', () => {
-    const { getByTestId, } = render(<Menu />);
+    const { getByTestId, } = render(
+      <HashRouter>
+        <Menu />
+      </HashRouter>
+    );
     expect(getByTestId('menu_block')).toBeInTheDocument();
+    expect(getByTestId('menu_list_block')).toBeInTheDocument();
+    expect(getByTestId('logo_block')).toBeInTheDocument();
+  });
+
+  test('Link 是否會正確改變路由', () => {
+    const route = '/';
+    const history = createMemoryHistory({ initialEntries: [route], });
+    const { getByTestId, } = render(
+      <Router history={history}>
+        <Menu />
+      </Router>
+    );
+    
+    expect(history.location.pathname).toBe('/');
+    fireEvent.click(getByTestId('menu_icon_todlist'));
+    expect(history.location.pathname).toBe('/todolist');
+
+    fireEvent.click(getByTestId('menu_icon_chart'));
+    expect(history.location.pathname).toBe('/todolist/chart');
+
+    fireEvent.click(getByTestId('menu_icon_ring'));
+    expect(history.location.pathname).toBe('/todolist/ring');
   });
 });
