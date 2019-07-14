282be2d072c596e4fff475b19e1b39956bb98847
GQSM <ms314006@hotmail.com>
2019-07-14 23:12:19 +0800

    [CHANGE] 主頁的待辦事項列下的 More 改 router 到 todolist/overview

--- src/component/Main/Information/Information.js
+++ src/component/Main/Information/Information.js
@@ -37,7 +37,7 @@ const Information = () => (
         <Link
           className={styles.clean_link_style}
           data-testid="todolist_link"
-          to="/todolist"
+          to="/todolist/overview"
         >
           MORE
         </Link>
