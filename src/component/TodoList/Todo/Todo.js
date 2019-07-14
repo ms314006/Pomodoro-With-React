ab421a1f9889fb440fa009ee535a638fba56dd2b
GQSM <ms314006@hotmail.com>
2019-07-14 23:16:33 +0800

    [CHANGE] 改成以 router 的位置判斷顯示的字體顏色

--- src/component/TodoList/Todo/Todo.js
+++ src/component/TodoList/Todo/Todo.js
@@ -4,14 +4,15 @@ import Circle from '../../Circle';
 import styles from './index.scss';
 
 const Todo = (props) => {
-  const { todo, } = props;
+  const { todo, match, } = props;
+  const isInnerPage = match.path.indexOf('todolist') !== -1;
   return (
     <div
-      className={styles.todo_block}
+      className={`${styles.todo_block} ${isInnerPage ? styles.innerPage_todo_block : ''}`}
       data-testid="todo_block"
     >
       <Circle style={{
-        border: '1px solid #003164', width: '24px', height: '24px', marginRight: '10px',
+        border: `1px solid ${isInnerPage ? '#FFFFFF' : '#003164'}`, width: '24px', height: '24px', marginRight: '10px',
       }}
       />
       <div className={styles.todo_name_block}>
@@ -26,10 +27,14 @@ const Todo = (props) => {
 
 Todo.propTypes = {
   todo: PropTypes.shape({}),
+  match: PropTypes.shape({}),
 };
 
 Todo.defaultProps = {
   todo: {},
+  match: {
+    path: '',
+  },
 };
 
 export default Todo;
